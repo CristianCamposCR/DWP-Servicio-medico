675538291be1668d7f0bfc80b4437dfaad920bcd
Cristian Alexis Campos Roman <20213tn003@utez.edu.mx>
2024-03-19 00:09:23 -0600

    [UPDATE] add validations for login and create account

--- client/src/modules/auth/views/LoginForm.vue
+++ client/src/modules/auth/views/LoginForm.vue
@@ -9,7 +9,21 @@
       <b-col sm="12">
         <b-form-group>
           <b-input-group class="mb-3">
-            <b-input placeholder="Correo Electronico"></b-input>
+            <b-input
+              placeholder="Correo Electronico"
+              type="email"
+              required
+              v-model.trim="v$.signin.email.$model"
+              trim
+              :state="v$.signin.email.$dirty ? !v$.signin.email.$error : null"
+              @blur="v$.signin.email.$touch()"
+            ></b-input>
+            <b-form-invalid-feedback
+              v-for="error in v$.signin.email.$errors"
+              :key="error.$uid"
+            >
+              {{ error.$message }}
+            </b-form-invalid-feedback>
           </b-input-group>
         </b-form-group>
         <b-form-group>
@@ -18,6 +32,13 @@
               :type="showPasswordState ? 'text' : 'password'"
               placeholder="Contraseña"
               style="border-right: none !important"
+              required
+              v-model.trim="v$.signin.password.$model"
+              trim
+              :state="
+                v$.signin.password.$dirty ? !v$.signin.password.$error : null
+              "
+              @blur="v$.signin.password.$touch()"
             >
             </b-form-input>
             <b-input-group-prepend>
@@ -32,6 +53,12 @@
                 ></b-icon>
               </span>
             </b-input-group-prepend>
+            <b-form-invalid-feedback
+              v-for="error in v$.signin.password.$errors"
+              :key="error.$uid"
+            >
+              {{ error.$message }}
+            </b-form-invalid-feedback>
           </b-input-group>
         </b-form-group>
       </b-col>
@@ -45,17 +72,41 @@
 </template>
 <script>
 import Vue from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, helpers } from "@vuelidate/validators";
 export default Vue.extend({
   name: "LoginForm",
+  setup() {
+    return {
+      v$: useVuelidate(),
+    };
+  },
   data() {
     return {
       showPasswordState: false,
+      signin: {
+        email: "",
+        password: "",
+      },
     };
   },
   methods: {
     showPassword() {
       this.showPasswordState = !this.showPasswordState;
     },
+  },
+  validations() {
+    return {
+      signin: {
+        email: {
+          required: helpers.withMessage("Campo obligatorio", required),
+          email: helpers.withMessage("Correo inválido", email),
+        },
+        password: {
+          required: helpers.withMessage("Campo obligatorio", required),
+        },
+      },
+    };
   },
 });
 </script>
