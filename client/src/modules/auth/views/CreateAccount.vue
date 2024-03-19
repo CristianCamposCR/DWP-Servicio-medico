c1ad7eca9b7ad1ea8c0d10c19887cd6061d4871a
Cristian Alexis Campos Roman <20213tn003@utez.edu.mx>
2024-03-19 00:09:23 -0600

    [UPDATE] add validations for login and create account

--- client/src/modules/auth/views/CreateAccount.vue
+++ client/src/modules/auth/views/CreateAccount.vue
@@ -8,8 +8,25 @@
     <b-row class="mx-4 mt-5">
       <b-col cols="12" sm="12" md="6">
         <b-form-group>
-          <b-form-input id="name" placeholder="Nombre" type="text" required>
+          <b-form-input
+            id="name"
+            placeholder="Nombre"
+            type="text"
+            required
+            v-model.trim="v$.newAccount.name.$model"
+            trim
+            :state="
+              v$.newAccount.name.$dirty ? !v$.newAccount.name.$error : null
+            "
+            @blur="v$.newAccount.name.$touch()"
+          >
           </b-form-input>
+          <b-form-invalid-feedback
+            v-for="error in v$.newAccount.name.$errors"
+            :key="error.$uid"
+          >
+            {{ error.$message }}
+          </b-form-invalid-feedback>
         </b-form-group>
       </b-col>
       <b-col cols="12" sm="12" md="6">
@@ -19,8 +36,22 @@
             placeholder="Primer apellido"
             type="text"
             required
+            v-model.trim="v$.newAccount.surname.$model"
+            trim
+            :state="
+              v$.newAccount.surname.$dirty
+                ? !v$.newAccount.surname.$error
+                : null
+            "
+            @blur="v$.newAccount.surname.$touch()"
           >
           </b-form-input>
+          <b-form-invalid-feedback
+            v-for="error in v$.newAccount.surname.$errors"
+            :key="error.$uid"
+          >
+            {{ error.$message }}
+          </b-form-invalid-feedback>
         </b-form-group>
       </b-col>
     </b-row>
@@ -38,8 +69,25 @@
       </b-col>
       <b-col cols="12" sm="12" md="6">
         <b-form-group>
-          <b-form-input id="phone" placeholder="Teléfono" type="text" required>
+          <b-form-input
+            id="phone"
+            placeholder="Teléfono"
+            type="text"
+            required
+            v-model.trim="v$.newAccount.phone.$model"
+            trim
+            :state="
+              v$.newAccount.phone.$dirty ? !v$.newAccount.phone.$error : null
+            "
+            @blur="v$.newAccount.phone.$touch()"
+          >
           </b-form-input>
+          <b-form-invalid-feedback
+            v-for="error in v$.newAccount.phone.$errors"
+            :key="error.$uid"
+          >
+            {{ error.$message }}
+          </b-form-invalid-feedback>
         </b-form-group>
       </b-col>
     </b-row>
@@ -51,8 +99,20 @@
             placeholder="Correo electrónico"
             type="email"
             required
+            v-model.trim="v$.newAccount.email.$model"
+            trim
+            :state="
+              v$.newAccount.email.$dirty ? !v$.newAccount.email.$error : null
+            "
+            @blur="v$.newAccount.email.$touch()"
           >
           </b-form-input>
+          <b-form-invalid-feedback
+            v-for="error in v$.newAccount.email.$errors"
+            :key="error.$uid"
+          >
+            {{ error.$message }}
+          </b-form-invalid-feedback>
         </b-form-group>
       </b-col>
     </b-row>
@@ -65,6 +125,15 @@
               :type="showPasswordState ? 'text' : 'password'"
               placeholder="Contraseña"
               style="border-right: none !important"
+              required
+              v-model.trim="v$.newAccount.password.$model"
+              trim
+              :state="
+                v$.newAccount.password.$dirty
+                  ? !v$.newAccount.password.$error
+                  : null
+              "
+              @blur="v$.newAccount.password.$touch()"
             >
             </b-form-input>
             <b-input-group-prepend>
@@ -79,6 +148,12 @@
                 ></b-icon>
               </span>
             </b-input-group-prepend>
+            <b-form-invalid-feedback
+              v-for="error in v$.newAccount.password.$errors"
+              :key="error.$uid"
+            >
+              {{ error.$message }}
+            </b-form-invalid-feedback>
           </b-input-group>
         </b-form-group>
       </b-col>
@@ -120,12 +195,32 @@
 
 <script>
 import Vue from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, helpers } from "@vuelidate/validators";
 export default Vue.extend({
   name: "CreateAccount",
+  setup() {
+    return {
+      v$: useVuelidate(),
+    };
+  },
   data() {
     return {
       showPasswordState: false,
       showConfirmPasswordState: false,
+      newAccount: {
+        name: "",
+        surname: "",
+        lastname: "",
+        phone: "",
+        email: "",
+        password: "",
+      },
+      confirmPassword: "",
+      errorMessagges: {
+        required: "Campo obligatorio",
+        invalidEmail: "Correo inválido",
+      },
     };
   },
   methods: {
@@ -135,6 +230,29 @@ export default Vue.extend({
     showConfirmPassword() {
       this.showConfirmPasswordState = !this.showConfirmPasswordState;
     },
+  },
+  validations() {
+    return {
+      newAccount: {
+        name: {
+          required: helpers.withMessage(this.errorMessagges.required, required),
+        },
+        surname: {
+          required: helpers.withMessage(this.errorMessagges.required, required),
+        },
+        phone: {
+          required: helpers.withMessage(this.errorMessagges.required, required),
+        },
+        email: {
+          required: helpers.withMessage(this.errorMessagges.required, required),
+          email: helpers.withMessage(this.errorMessagges.invalidEmail, email),
+        },
+        password: {
+          required: helpers.withMessage(this.errorMessagges.required, required),
+        },
+      },
+      confirmPassword: {},
+    };
   },
 });
 </script>
