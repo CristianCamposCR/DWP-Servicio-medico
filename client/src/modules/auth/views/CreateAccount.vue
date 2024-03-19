4075a1995ba17499fd9f1e470d73680d0c6c30f0
Cristian Alexis Campos Roman <20213tn003@utez.edu.mx>
2024-03-18 23:11:29 -0600

    [UPDATE] resolve login tabs and styles

--- client/src/modules/auth/views/CreateAccount.vue
+++ client/src/modules/auth/views/CreateAccount.vue
@@ -1,73 +1,140 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center vh-100">
-    <b-tabs content-class="mt-3">
-      <b-tab @click="navigateLogin" title="Iniciar Sesión"></b-tab>
-      <b-tab title="Crear Cuenta" active>
-        <b-card :img-src="require('@/assets/Hospital.jpg')" img-alt="Card image" img-top>
-          <div class="px-5 mx-5">
-            <b-form-group>
-              <b-row>
-                <b-col>
-                  <b-input-group class="mb-3">
-                    <b-input placeholder="Nombre"></b-input>
-                  </b-input-group>
-                </b-col>
-                <b-col>
-                  <b-input-group class="mb-3">
-                    <b-input placeholder="Apellidos"></b-input>
-                  </b-input-group>
-                </b-col>
-              </b-row>
-              <b-input-group class="mb-3">
-                <b-input placeholder="Correo Electronico"></b-input>
-              </b-input-group>
-              <b-input-group class="mb-3">
-                <b-input placeholder="Teléfono" type="number"></b-input>
-              </b-input-group>
-              <b-input-group class="mb-3">
-                <b-input placeholder="Password" type="password"></b-input>
-              </b-input-group>
-              <b-input-group class="mb-3">
-                <b-input placeholder="Confirmar Password"></b-input>
-              </b-input-group>
-            </b-form-group>
-            <b-row class="custom-row">
-              <b-col cols="12" sm="12" md="4">
-                <b-button class="custom-button">Crear Cuenta</b-button>
-              </b-col>
-            </b-row>
-          </div>
-        </b-card>
-      </b-tab>
-      <router-view></router-view>
-    </b-tabs>
+  <div>
+    <b-card-img
+      src="/src/assets/Hospital.jpg"
+      width="100px"
+      height="140px"
+    ></b-card-img>
+    <b-row class="mx-4 mt-5">
+      <b-col cols="12" sm="12" md="6">
+        <b-form-group>
+          <b-form-input id="name" placeholder="Nombre" type="text" required>
+          </b-form-input>
+        </b-form-group>
+      </b-col>
+      <b-col cols="12" sm="12" md="6">
+        <b-form-group>
+          <b-form-input
+            id="surname"
+            placeholder="Primer apellido"
+            type="text"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row class="mx-4">
+      <b-col cols="12" sm="12" md="6">
+        <b-form-group>
+          <b-form-input
+            id="lastname"
+            placeholder="Segundo apellido"
+            type="text"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+      </b-col>
+      <b-col cols="12" sm="12" md="6">
+        <b-form-group>
+          <b-form-input id="phone" placeholder="Teléfono" type="text" required>
+          </b-form-input>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row class="mx-4">
+      <b-col cols="12" sm="12">
+        <b-form-group>
+          <b-form-input
+            id="email"
+            placeholder="Correo electrónico"
+            type="email"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row class="mx-4">
+      <b-col cols="12" sm="12">
+        <b-form-group>
+          <b-input-group>
+            <b-form-input
+              id="password"
+              :type="showPasswordState ? 'text' : 'password'"
+              placeholder="Contraseña"
+              style="border-right: none !important"
+            >
+            </b-form-input>
+            <b-input-group-prepend>
+              <span
+                class="input-group-text"
+                style="background-color: white; border-left: none !important"
+                @click="showPassword"
+                role="button"
+              >
+                <b-icon
+                  :icon="showPasswordState ? 'eye-slash' : 'eye'"
+                ></b-icon>
+              </span>
+            </b-input-group-prepend>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row class="mx-4">
+      <b-col cols="12" sm="12">
+        <b-form-group>
+          <b-input-group>
+            <b-form-input
+              id="confirmPassword"
+              :type="showConfirmPasswordState ? 'text' : 'password'"
+              placeholder="Confirmar contraseña"
+              style="border-right: none !important"
+            >
+            </b-form-input>
+            <b-input-group-prepend>
+              <span
+                class="input-group-text"
+                style="background-color: white; border-left: none !important"
+                @click="showConfirmPassword"
+                role="button"
+              >
+                <b-icon
+                  :icon="showConfirmPasswordState ? 'eye-slash' : 'eye'"
+                ></b-icon>
+              </span>
+            </b-input-group-prepend>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row class="d-flex justify-content-center mb-5 mx-4">
+      <b-col cols="12" sm="12" md="6">
+        <b-button class="custom-button" block> Crear cuenta </b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-export default {
+import Vue from "vue";
+export default Vue.extend({
+  name: "CreateAccount",
+  data() {
+    return {
+      showPasswordState: false,
+      showConfirmPasswordState: false,
+    };
+  },
   methods: {
-    navigateLogin() {
-      this.$router.push({
-        name: "login",
-      });
+    showPassword() {
+      this.showPasswordState = !this.showPasswordState;
+    },
+    showConfirmPassword() {
+      this.showConfirmPasswordState = !this.showConfirmPasswordState;
     },
   },
-};
+});
 </script>
-
-<style>
-.custom-row {
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-}
-
-.custom-button {
-  background-color: #10b981;
-  border-color: #10b981;
-  color: white;
-  font-size: 20px;
-  padding: 10px 20px;
-}
-</style>
---
+++ client/src/modules/auth/views/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-card-img
+      src="/src/assets/Hospital.jpg"
+      width="100px"
+      height="140px"
+    ></b-card-img>
+    <div class="mt-5 mx-4">
+      <b-col sm="12">
+        <b-form-group>
+          <b-input-group class="mb-3">
+            <b-input placeholder="Correo Electronico"></b-input>
+          </b-input-group>
+        </b-form-group>
+        <b-form-group>
+          <b-input-group>
+            <b-form-input
+              :type="showPasswordState ? 'text' : 'password'"
+              placeholder="Contraseña"
+              style="border-right: none !important"
+            >
+            </b-form-input>
+            <b-input-group-prepend>
+              <span
+                class="input-group-text"
+                style="background-color: white; border-left: none !important"
+                @click="showPassword"
+                role="button"
+              >
+                <b-icon
+                  :icon="showPasswordState ? 'eye-slash' : 'eye'"
+                ></b-icon>
+              </span>
+            </b-input-group-prepend>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+    </div>
+    <div class="d-flex justify-content-center mt-3 mb-5 mx-4">
+      <b-col cols="12" sm="6">
+        <b-button block class="custom-button"> Iniciar sesión </b-button>
+      </b-col>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "LoginForm",
+  data() {
+    return {
+      showPasswordState: false,
+    };
+  },
+  methods: {
+    showPassword() {
+      this.showPasswordState = !this.showPasswordState;
+    },
+  },
+});
+</script>
