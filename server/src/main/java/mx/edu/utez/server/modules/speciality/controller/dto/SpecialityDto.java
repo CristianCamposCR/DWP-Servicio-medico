77c35f45599d474df44ed93aac6ae3851adeae8e
PabloSFlores <i20213tn013@utez.edu.mx>
2024-03-20 14:50:54 -0600

    [UPDATE] change lombook annotation

--- server/src/main/java/mx/edu/utez/server/modules/speciality/controller/dto/SpecialityDto.java
+++ server/src/main/java/mx/edu/utez/server/modules/speciality/controller/dto/SpecialityDto.java
@@ -1,13 +1,7 @@
 package mx.edu.utez.server.modules.speciality.controller.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class SpecialityDto {
 }
