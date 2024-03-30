147a729cf518bbb0dd0aea620d5778e7496e3b0f
PabloSFlores <i20213tn013@utez.edu.mx>
2024-03-29 19:42:09 -0600

    [ADD] list all appointment types service

--- server/src/main/java/mx/edu/utez/server/modules/appointmentType/model/IAppointmentTypeRepository.java
+++ server/src/main/java/mx/edu/utez/server/modules/appointmentType/model/IAppointmentTypeRepository.java
@@ -1,11 +1,15 @@
 package mx.edu.utez.server.modules.appointmentType.model;
 
+import mx.edu.utez.server.kernel.Statuses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface IAppointmentTypeRepository extends JpaRepository<AppointmentType, Long> {
     Optional<AppointmentType> findByName(String name);
+
+    Set<AppointmentType> findAllByStatus_Name(Statuses statusName);
 }
