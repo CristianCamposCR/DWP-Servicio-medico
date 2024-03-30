8dcd454bb26971a26787ab203f5b5c06e6a02355
PabloSFlores <i20213tn013@utez.edu.mx>
2024-03-29 19:52:56 -0600

    [ADD] list all gender service

--- server/src/main/java/mx/edu/utez/server/modules/gender/service/GenderService.java
+++ server/src/main/java/mx/edu/utez/server/modules/gender/service/GenderService.java
@@ -1,15 +1,20 @@
 package mx.edu.utez.server.modules.gender.service;
 
 import lombok.RequiredArgsConstructor;
+import mx.edu.utez.server.kernel.Errors;
 import mx.edu.utez.server.kernel.Genders;
+import mx.edu.utez.server.kernel.Statuses;
 import mx.edu.utez.server.modules.gender.model.Gender;
 import mx.edu.utez.server.modules.gender.model.IGenderRepository;
 import mx.edu.utez.server.modules.status.model.Status;
+import mx.edu.utez.server.utils.ResponseApi;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -21,5 +26,24 @@ public class GenderService {
     public Gender findOrSave(Genders name, Status status) {
         Optional<Gender> optionalGender = this.iGenderRepository.findByName(name);
         return optionalGender.orElseGet(() -> this.iGenderRepository.saveAndFlush(new Gender(name, status)));
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseApi<Set<Gender>> openListAll() {
+        try {
+            Set<Gender> genders = this.iGenderRepository.findAllByStatus_Name(Statuses.ACTIVO);
+            return new ResponseApi<>(
+                    genders,
+                    HttpStatus.OK,
+                    false,
+                    "Lista de tipos de cita."
+            );
+        } catch (Exception e) {
+            return new ResponseApi<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    true,
+                    Errors.SERVER_ERROR.name()
+            );
+        }
     }
 }
