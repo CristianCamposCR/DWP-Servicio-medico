025d6c8f838346c8a88ff7b20a07846547325e37
PabloSFlores <i20213tn013@utez.edu.mx>
2024-03-29 20:04:11 -0600

    [ADD] list all shifts service

--- server/src/main/java/mx/edu/utez/server/modules/shift/service/ShiftService.java
+++ server/src/main/java/mx/edu/utez/server/modules/shift/service/ShiftService.java
@@ -1,15 +1,20 @@
 package mx.edu.utez.server.modules.shift.service;
 
 import lombok.RequiredArgsConstructor;
+import mx.edu.utez.server.kernel.Errors;
 import mx.edu.utez.server.kernel.Shifts;
+import mx.edu.utez.server.kernel.Statuses;
 import mx.edu.utez.server.modules.shift.model.IShiftRepository;
 import mx.edu.utez.server.modules.shift.model.Shift;
 import mx.edu.utez.server.modules.status.model.Status;
+import mx.edu.utez.server.utils.ResponseApi;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -21,5 +26,24 @@ public class ShiftService {
     public Shift findOrSave(Shifts name, Integer entryHour, Integer departureHour, Status status) {
         Optional<Shift> optionalShift = this.iShiftRepository.findByName(name);
         return optionalShift.orElseGet(() -> this.iShiftRepository.saveAndFlush(new Shift(name, entryHour, departureHour, status)));
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseApi<Set<Shift>> openListAll() {
+        try {
+            Set<Shift> shifts = this.iShiftRepository.findAllByStatus_Name(Statuses.ACTIVO);
+            return new ResponseApi<>(
+                    shifts,
+                    HttpStatus.OK,
+                    false,
+                    "Lista de tipos de cita."
+            );
+        } catch (Exception e) {
+            return new ResponseApi<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    true,
+                    Errors.SERVER_ERROR.name()
+            );
+        }
     }
 }
