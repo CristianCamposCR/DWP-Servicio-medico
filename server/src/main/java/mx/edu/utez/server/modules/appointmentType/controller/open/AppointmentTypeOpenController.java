9ad7fe015e35797316cee6fe6469c3efd5f2a8be
PabloSFlores <i20213tn013@utez.edu.mx>
2024-03-29 19:42:09 -0600

    [ADD] list all appointment types service

---
+++ server/src/main/java/mx/edu/utez/server/modules/appointmentType/controller/open/AppointmentTypeOpenController.java
@@ -0,0 +1,27 @@
+package mx.edu.utez.server.modules.appointmentType.controller.open;
+
+import lombok.RequiredArgsConstructor;
+import mx.edu.utez.server.modules.appointmentType.model.AppointmentType;
+import mx.edu.utez.server.modules.appointmentType.service.AppointmentTypeService;
+import mx.edu.utez.server.utils.ResponseApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping(value = "/api/open/appointment-type")
+@CrossOrigin(origins = {"*"})
+@RequiredArgsConstructor
+public class AppointmentTypeOpenController {
+    private final AppointmentTypeService appointmentTypeService;
+
+    @GetMapping("/")
+    public ResponseEntity<ResponseApi<Set<AppointmentType>>> listAll() {
+        ResponseApi<Set<AppointmentType>> responseApi = this.appointmentTypeService.openListAll();
+        return new ResponseEntity<>(responseApi, responseApi.getStatus());
+    }
+}
--- server/src/main/java/mx/edu/utez/server/modules/appointmentType/service/AppointmentTypeService.java
+++ server/src/main/java/mx/edu/utez/server/modules/appointmentType/service/AppointmentTypeService.java
@@ -1,14 +1,19 @@
 package mx.edu.utez.server.modules.appointmentType.service;
 
 import lombok.RequiredArgsConstructor;
+import mx.edu.utez.server.kernel.Errors;
+import mx.edu.utez.server.kernel.Statuses;
 import mx.edu.utez.server.modules.appointmentType.model.AppointmentType;
 import mx.edu.utez.server.modules.appointmentType.model.IAppointmentTypeRepository;
 import mx.edu.utez.server.modules.status.model.Status;
+import mx.edu.utez.server.utils.ResponseApi;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -20,5 +25,24 @@ public class AppointmentTypeService {
     public AppointmentType findOrSave(String name, String description, Status status) {
         Optional<AppointmentType> optionalAppointmentType = this.iAppointmentTypeRepository.findByName(name);
         return optionalAppointmentType.orElseGet(() -> this.iAppointmentTypeRepository.saveAndFlush(new AppointmentType(name, description, status)));
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseApi<Set<AppointmentType>> openListAll() {
+        try {
+            Set<AppointmentType> appointmentTypes = this.iAppointmentTypeRepository.findAllByStatus_Name(Statuses.ACTIVO);
+            return new ResponseApi<>(
+                    appointmentTypes,
+                    HttpStatus.OK,
+                    false,
+                    "Lista de tipos de cita."
+            );
+        } catch (Exception e) {
+            return new ResponseApi<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    true,
+                    Errors.SERVER_ERROR.name()
+            );
+        }
     }
 }
