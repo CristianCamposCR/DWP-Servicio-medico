f31f7769291bc7ad038dd749a65109ccdb139ca4
PabloSFlores <i20213tn013@utez.edu.mx>
2024-04-02 13:55:59 -0600

    [ADD] Jasypt implementation for properties values

--- server/src/main/java/mx/edu/utez/server/ServerApplication.java
+++ server/src/main/java/mx/edu/utez/server/ServerApplication.java
@@ -1,10 +1,12 @@
 package mx.edu.utez.server;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ServerApplication {
 
