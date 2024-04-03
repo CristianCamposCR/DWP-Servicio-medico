4c7d0fb52cebd04d8a5c32d63a113ec72196ba4f
AlejandroMorellano <20213tn026@utez.edu.mx>
2024-04-03 01:24:16 -0600

    [ADD] getAll and save speciality

---
+++ client/src/modules/management/speciality/services/controller/speciality.controller.js
@@ -0,0 +1,81 @@
+import ApiService from "../service/speciality.gateway";
+
+export default {
+  async getAllSpecialties(pagination) {
+    try {
+      const data = await ApiService.getAllSpecialties(pagination);
+      return data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+
+  async saveSpeciality(payload) {
+    try {
+      const data = await ApiService.saveSpeciality(payload);
+      return data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+
+  async changeStatus(payload) {
+    try {
+      const data = await ApiService.chageStatus(payload);
+      return data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+
+  async getOne(payload) {
+    try {
+      const data = await ApiService.getOne(payload);
+      return data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+
+  async updateSpeciality(payload) {
+    try {
+      const data = await ApiService.updateSpeciality(payload);
+      return data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+
+  async getAreas() {
+    try {
+      const data = await ApiService.getAreas();
+      return data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+};
