5c29f16360a83f4362596f7d6de3fd4f96aa8b47
PabloSFlores <i20213tn013@utez.edu.mx>
2024-04-07 14:27:07 -0600

    [ADD] Person services

--- server/src/main/java/mx/edu/utez/server/modules/person/model/IPersonRepository.java
+++ server/src/main/java/mx/edu/utez/server/modules/person/model/IPersonRepository.java
@@ -1,5 +1,6 @@
 package mx.edu.utez.server.modules.person.model;
 
+import mx.edu.utez.server.kernel.Statuses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,17 @@ public interface IPersonRepository extends JpaRepository<Person, Long> {
             "OR curp = ?2 " +
             "OR email = ?3", nativeQuery = true)
     Optional<Person> findByFullnameOrCurpOrEmail(String fullname, String curp, String email);
+
+    Optional<Person> findByUser_UsernameIgnoreCase(String email);
+
+    @Query(value = "SELECT * " +
+            "FROM people " +
+            "WHERE (LOWER(TRIM(CONCAT(name, ' ', surname, COALESCE(' ' || lastname, '')))) = LOWER(?1) " +
+            "OR curp = ?2 " +
+            "OR email = ?3) " +
+            "AND id <> ?4", nativeQuery = true)
+    Optional<Person> findByFullnameOrCurpOrEmailAndIdNot(String fullname, String curp, String email, Long id);
+
+    Optional<Person> findByIdAndUser_Status_NameNot(Long id, Statuses statusName);
+    boolean existsByIdAndUser_Status_NameNot(Long id, Statuses statusName);
 }
