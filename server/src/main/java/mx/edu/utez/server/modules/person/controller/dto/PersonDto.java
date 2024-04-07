64625c3ccabdd0738e8dd561ca754c5c4f5a2e9f
PabloSFlores <i20213tn013@utez.edu.mx>
2024-04-07 14:27:07 -0600

    [ADD] Person services

--- server/src/main/java/mx/edu/utez/server/modules/person/controller/dto/PersonDto.java
+++ server/src/main/java/mx/edu/utez/server/modules/person/controller/dto/PersonDto.java
@@ -1,7 +1,69 @@
 package mx.edu.utez.server.modules.person.controller.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import mx.edu.utez.server.modules.gender.controller.dto.GenderDto;
+import mx.edu.utez.server.modules.person.model.Person;
+
+import java.time.LocalDate;
 
 @Data
 public class PersonDto {
+    @NotNull
+    @Positive
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String surname;
+
+    @Size(max = 50)
+    private String lastname;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String email;
+
+    @Size(min = 18, max = 18)
+    private String curp;
+
+    @Size(min = 10, max = 15)
+    private String phoneNumber;
+
+    private String details;
+
+    @Past
+    private LocalDate birthday;
+
+    private String profilePhoto;
+
+    // Relationships <-
+    @NotNull
+    @Valid
+    private GenderDto gender;
+
+    public Person getPersonEntity() {
+        return new Person(
+                getId(),
+                getName(),
+                getSurname(),
+                getLastname(),
+                getEmail(),
+                getCurp(),
+                getPhoneNumber(),
+                getDetails(),
+                getBirthday(),
+                getProfilePhoto(),
+                getGender().getGenderEntity()
+        );
+    }
 }
