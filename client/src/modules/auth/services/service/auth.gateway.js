587bfca80df1af8ddf6ebdc0cc75104fbed74ad1
Cristian Campos <20213tn003@utez.edu.mx>
2024-04-11 17:40:52 -0600

    [ADD] signup and verification account

--- client/src/modules/auth/services/service/auth.gateway.js
+++ client/src/modules/auth/services/service/auth.gateway.js
@@ -21,9 +21,31 @@ export default {
   async singup(payload) {
     try {
       const encryptPassword = await encrypt(payload.password);
-      const newPayload = { ...payload, password: encryptPassword };
+      const newPhonewNumber = `+52${payload.phoneNumber}`;
+      const newPayload = {
+        ...payload,
+        password: encryptPassword,
+        phoneNumber: newPhonewNumber,
+      };
       const response = await axios.doPost("/auth/signup/", newPayload);
       return response.data.data;
+    } catch (error) {
+      return {
+        code: error.data?.code,
+        error: true,
+        message: error.data?.message,
+      };
+    }
+  },
+
+  async activateAccount(payload) {
+    try {
+      const response = await axios.doPost(
+        "/open/verification-code/activate_account/",
+        { code: payload }
+      );
+      console.log("gate", response);
+      return response.data;
     } catch (error) {
       return {
         code: error.data?.code,
