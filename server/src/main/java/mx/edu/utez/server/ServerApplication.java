65d362fd2df1f56be601bb6d1b0b8ec683aa1580
alexxjoel <20213tn019@utez.edu.mx>
2024-04-14 13:02:12 -0600

    [ADD] Save appointment

--- server/src/main/java/mx/edu/utez/server/ServerApplication.java
+++ server/src/main/java/mx/edu/utez/server/ServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @SpringBootApplication
-public class ServerApplication {
+    public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
