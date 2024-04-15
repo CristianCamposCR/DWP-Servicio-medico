22e0823e859ffb9a200b9ac23e8cc9656dee6b99
CarmonaMedinaMaycon <105668860+CarmonaMedinaMaycon@users.noreply.github.com>
2024-04-14 23:28:27 -0600

    [ADD] front appointments-list

--- client/src/views/components/DoctorNavbar.vue
+++ client/src/views/components/DoctorNavbar.vue
@@ -1,70 +1,10 @@
 <template>
-<<<<<<< HEAD
   <div class="sticky-top">
     <b-navbar type="light " variant="white" class="shadow-sm">
       <b-navbar-brand to="/doctor" class="custom-nav-font">
         <b-img src="https://picsum.photos/50/50/?image=41"></b-img>
         CIMI</b-navbar-brand
       >
-=======
-    <div class="sticky-top">
-      <b-navbar type="light " variant="white" class="shadow-sm">
-        <b-navbar-brand to="/doctor" class="custom-nav-font">
-          <b-img src="https://picsum.photos/50/50/?image=41"></b-img>
-          CIMI</b-navbar-brand
-        >
-  
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-  
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item
-              v-for="link in links"
-              :key="link.name"
-              :to="link.to"
-              class="custom-nav-font"
-            >
-              {{ link.name }}
-            </b-nav-item>
-            <b-nav-item-dropdown right>
-            <template #button-content>
-             <b class="custom-nav-font">Citas </b>
-              <b-icon icon="calendar"> </b-icon>
-            </template>
-            <b-dropdown-item to="appointments-active-doctor">Citas Asignadas</b-dropdown-item>
-            <b-dropdown-item to="appointments-history-doctor">Historial de citas</b-dropdown-item>
-          </b-nav-item-dropdown>
-          </b-navbar-nav>
-          <!-- Right aligned nav items -->
-  
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <b-avatar
-                  variant="secondary"
-                  size="md"
-                  icon="person-fill"
-                ></b-avatar>
-              </template>
-              <b-dropdown-item @click="profile()"
-                >Perfil</b-dropdown-item
-              >
-              <b-dropdown-item @click="logout()">Cerrar sesión</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
-  </template>
-  <script>
-  import Vue from "vue";
-  export default Vue.extend({
-    name: "DoctorNavbar",
-    data() {
-      return {
-        links: [
->>>>>>> 94fea5e ([ADD] front appointments-list)
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -79,6 +19,14 @@
           >
             {{ link.name }}
           </b-nav-item>
+          <b-nav-item-dropdown right>
+            <template #button-content>
+             <b class="custom-nav-font">Citas </b>
+              <b-icon icon="calendar"> </b-icon>
+            </template>
+            <b-dropdown-item to="appointments-active-doctor">Citas Asignadas</b-dropdown-item>
+            <b-dropdown-item to="appointments-history-doctor">Historial de citas</b-dropdown-item>
+          </b-nav-item-dropdown>
         </b-navbar-nav>
         <!-- Right aligned nav items -->
 
